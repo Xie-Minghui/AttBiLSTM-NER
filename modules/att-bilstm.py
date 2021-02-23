907c6ee0757605797f1a65f7b6f022bef98ec334
mango <2075419247@qq.com>
2021-02-23 12:03:35 +0800

    添加atten-bilstm的代码和配置文件的代码

---
+++ modules/att-bilstm.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+
+# @Author  : xmh
+# @Time    : 2021/2/23 10:36
+# @File    : att-bilstm.py
+
+"""
+file description:：
+
+"""
+import torch
+import torch.nn as nn
+
+
+class Attention(nn.Modules):
+    def __init__(self, config):
+        self.query = nn.Parameter(torch.randn(config.batch_size, 1, config.hidden_dim_lstm))  # [batch, 1, hidden_dim]
+    
+    def forward(self, H):
+        M = torch.tanh(H)  # H [batch_size, sentence_length, hidden_dim_lstm]
+        attention_prob = torch.matmul(M, self.query.transpose(-1,-2))  # [batch_size, sentence_length, 1]
+        alpha = nn.Softmax(attention_prob)
+        attention_output = torch.matmul(alpha.transpose(-1,-2), H)  # [batch_size, 1, hidden_dim_lstm]
+        
+        return attention_output
+        
+        
+class AttBiLSTM(nn.Modules):
+    def __init__(self, config):
+        self.embedding_dim = config.embedding_dim
+        self.vocab_size = config.vocab_size
+        self.hidden_dim = config.hidden_dim_lstm
+        self.num_layers = config.num_layers
+        self.batch_size = config.batch_size
+        
+        # 定义网络层
+        self.word_embedding = nn.Embedding(config.vocab_size, config.embedding_dim, padding_idx=config.pad_token_id)
+        self.gru = nn.GRU(config.embedding_dim, config.hidden_dim_lstm, batch_first=True, bidirectional=True)
+        self.attention_layer = Attention(config)
+        
+    def forward(self, x):
+        embeddings = self.word_embedding(x)  # [batch_size, sentence_length, embedding_dim]
+        hidden_init = torch.randn(2*self.num_layers, self.batch_size, self.hidden_dim)
+        output, h_n = self.gru(embeddings, hidden_init)
+        attention_output = self.attention_layer(output)
+        #hidden_cls = torch.tanh(attention_output)
+        
+        return attention_output
+        
+        
+        
+        
+        
+        
+        
+
---
+++ utils/config.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+# @Author  : xmh
+# @Time    : 2021/2/23 10:42
+# @File    : config.py.py
+
+"""
+file description:：
+
+"""
+
+
+class LSTMConfig:
+    def __init__(self,
+                 embedding_dim=128,
+                 vocab_size=512,
+                 pad_token_id=0,
+                 hidden_dim_lstm=128,
+                 batch_size=8,
+                 num_layers=1
+                 ):
+        
+        self.embedding_dim = embedding_dim
+        self.vocab_size = vocab_size
+        self.pad_token_id = pad_token_id
+        self.hidden_dim_lstm = hidden_dim_lstm
+        self.batch_size = batch_size
+        self.num_layers = num_layers
