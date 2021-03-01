03a101b324a695d9808a79a89f2b42d219e8c276
mango <2075419247@qq.com>
2021-03-01 21:14:37 +0800

    添加通过relation embed与attention权重相乘得到的分类结果

--- modules/att_biLSTM_NER.py
+++ modules/att_biLSTM_NER.py
@@ -11,6 +11,8 @@ file description:：
 import torch
 import torch.nn as nn
 from .att_biLSTM import AttBiLSTM
+from utils.config import args
+import torch.nn.functional as F
 
 
 class AttBiLSTM4NER(nn.Modules):
@@ -18,12 +20,22 @@ class AttBiLSTM4NER(nn.Modules):
     def __init__(self, config):
         self.att_biLSTM_layer = AttBiLSTM(config)
         self.classifier = nn.Linear(config.hidden_dim_lstm, config.tag_size)
+        self.tag_size = config.tag_size
+        self.relation_embedding = nn.Embedding(config.tag_size, config.hidden_dim_lstm)
+        self.relation_bias = nn.Parameter(torch.randn(args['batch_size'], config.tag_size, 1))
     
     def forward(self, x):
-        attention_output = self.att_biLSTM_layer(x)
-        hidden4cls = torch.tanh(attention_output)
-        logits = self.classifier(hidden4cls)
-        logsoft = nn.Softmax(logits)
+        attention_output = self.att_biLSTM_layer(x)  # [batch_size, 1, hidden_dim_lstm]
+        
+        # 下面是论文的做法
+        # hidden4cls = F.tanh(attention_output)
+        # logits = self.classifier(hidden4cls)
+        # logsoft = nn.Softmax(logits)
+        
+        attention_output = F.tanh(attention_output)
+        relation = torch.tensor([i for i in range(self.tag_size)], dtype=torch.long).repeat(args['batch_size'], 1)
+        relation = self.relation_embedding(relation)
+        res = torch.add(torch.bmm(attention_output, relation.transpose(-1,-2)), self.relation_bias)
+        logsoft = F.softmax(res, 1)
         
         return logsoft
-        
